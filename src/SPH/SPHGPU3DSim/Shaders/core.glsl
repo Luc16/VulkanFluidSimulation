a62afeeb31f8c6b2ef37523b8a1be6fa67a07321
Luc Joffily Ribas <luc.joffily.ribas@gmail.com>
2023-06-03 21:22:51 -0300

    fixed release build command

---
+++ src/SPH/SPHGPU3DSim/Shaders/core.glsl
@@ -0,0 +1,32 @@
+#ifndef CORE_SHADER_H
+#define CORE_SHADER_H
+
+
+struct Particle {
+    vec3 position, velocity, force;
+    float density, pressure;
+    vec4 color;
+};
+
+struct computeUBO {
+    float deltaTime;
+    float BOUNDARY_SIZE;
+    float planeY;
+
+    vec3 G;   // external (gravitational) forces
+    float REST_DENS;  // rest density
+    float GAS_CONST; // const for equation of state
+    float H;           // kernel radius
+    float HSQ;        // radius^2 for optimization
+    float MASS;        // assume all particles have the same mass
+    float VISC;       // viscosity constant
+    float DT;       // integration timestep
+    float POLY6;
+    float SPIKY_GRAD;
+    float VISC_LAP;
+    float EPS;
+    float BOUND_DAMPING;
+    uint numParticles;
+};
+
+#endif //CORE_SHADER_H
